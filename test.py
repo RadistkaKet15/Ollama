c486e324bd5868ffda0988c219ef2d8f735c2ace
RadistkaKet15 <ekaterina.gladilkina@yandex.ru>
2025-07-11 11:13:25 +0300

    final worked version

--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 from ollama_ocr import OCRProcessor
+import time
 
+start = time.time()
 # Initialize OCR processor
 ocr = OCRProcessor(model_name='granite3.2-vision', base_url="http://ollama:11434/api/generate")  # You can use any vision model available on Ollama
 # you can pass your custom ollama api
 
 # Process an image
 result = ocr.process_image(
-    image_path="/app/Книга 4.4.pdf", # path to your pdf files "path/to/your/file.pdf"
+    image_path="/app/img.pdf", # path to your pdf files "path/to/your/file.pdf"
     format_type="markdown",  # Options: markdown, text, json, structured, key_value
-    #custom_prompt="Извлечь весь текстовый контент из этого изображения на русском языке **точно так, как он выглядит**, без изменений, обобщений или пропусков.", # Optional custom prompt
+    #custom_prompt="Распознай текст максимально точно, сохраняя сходное форматирование. Не изменяй и не перефразируй текст - просто извлеки его в том виде, в котором он представлен", # Optional custom prompt
     language="Russian" # Specify the language of the text (New! )
 )
 print(result)
@@ -21,3 +23,4 @@ try:
     print(f"Результат успешно сохранён в файл: {output_file_path}")
 except Exception as e:
     print(f"Ошибка при сохранении файла: {e}")
+print(f"Time: {time.time() - start}")
